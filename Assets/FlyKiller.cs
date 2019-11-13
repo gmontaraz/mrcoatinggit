5af1aa474a07db3067ca3145ff6303590f200832
gmontaraz <fayderlh@gmail.com>
2019-11-13 01:16:48 +0100

    empuje enemigo

--- Assets/FlyKiller.cs
+++ Assets/FlyKiller.cs
@@ -14,23 +14,31 @@ public class FlyKiller : MonoBehaviour
     void Update()
     {
         RaycastHit2D[] enemies_inside = Physics2D.CircleCastAll(transform.position, 0.75f,Vector2.zero);
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X) && attack_speed<=0)
         {
             Debug.Log("lol");
             int i;
-            
+            attack_speed = 0.3f;
             for (i = 0; i < enemies_inside.Length; i++)
             {
                 if (enemies_inside[i].collider.CompareTag("Enemy") || enemies_inside[i].collider.CompareTag("Spider"))
                 {
                     Debug.Log("xd");
                     enemies_inside[i].collider.gameObject.GetComponent<EnemyHealth>().attacked();
-                    //enemies_inside[i].collider.gameObject.transform.Translat
+                    Vector2 angle = enemies_inside[i].collider.gameObject.transform.position - transform.position;
+                    angle=angle.normalized;
+                    Debug.Log(angle);
+                    enemies_inside[i].collider.gameObject.transform.Translate(angle );
                     cam_anim.SetTrigger("Shake");
                 }
             }
         }
+        if (attack_speed > 0)
+        {
+            attack_speed -= Time.deltaTime;
+        }
         
     }
     public Animator cam_anim;
+    [SerializeField] private float attack_speed;
 }
